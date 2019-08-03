b00e5feca4f3ff555ff2611f3eac15ad478ee62a
Caglar Cavdar <chaglare@gmail.com>
2019-08-03 13:07:19 -0500

    edited

--- instance_mysql.tf
+++ instance_mysql.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "mysql" {
     ami                         = "${var.ami}"
     instance_type               = "${var.instance_type}"
     key_name                    = "${var.key_name}"
-    subnet_id                   = "${aws_subnet.privateDB.id}"
+    subnet_id                   = "${aws_subnet.private.id}"
     user_data                   = "${file("userdata.sh")}"
     vpc_security_group_ids     = ["${aws_security_group.privateDB.id}"]
     
