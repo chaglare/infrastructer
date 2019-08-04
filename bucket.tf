44ec8a41c96be52a958007cb7b5d7b089e895dbc
Caglar Cavdar <chaglare@gmail.com>
2019-08-04 14:45:09 -0500

    module inst. added

--- bucket.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "bucketname" {
-    bucket = "insfrastructer-april-caglar-2019"
-  
-}
--- dont_create.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_s3_bucket" "bucketnameb" {
-    bucket = "insfrastructer-april-caglar-2019b"
-  
-}
