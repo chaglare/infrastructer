c4c784a18dd5bd7c2f2cfd911c6661afc3a83fa6
Caglar Cavdar <chaglare@gmail.com>
2019-08-04 16:27:50 -0500

    edited

--- asg_db.tf
+++ asg_db.tf
@@ -7,12 +7,12 @@ module "mysql" {
   lc_name = "mysql-lc"
   image_id        = "${var.ami}"
   instance_type   = "${var.instance_type}"
-  security_groups = ["${aws_security_group.private.id}"]
+  security_groups = ["${aws_security_group.privateDB.id}"]
 
  
   # Auto scaling group
   asg_name                  = "mysql-asg"
-  vpc_zone_identifier       = ["${aws_subnet.private.id}"]
+  vpc_zone_identifier       = ["${aws_subnet.privateDB.id}"]
   health_check_type         = "EC2"
   min_size                  = 3
   max_size                  = 6
