141cee2d287eb996f344df1b0d2e03c8b3fb65f7
Caglar Cavdar <chaglare@gmail.com>
2019-08-04 17:16:30 -0500

    edited

--- asg_db.tf
+++ asg_db.tf
@@ -1,29 +1,28 @@
-module "mysql" {
+module "wordpress" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "3.0"
-  name = "mysql"
+  name = "${var.appname}"
+
 
   # Launch configuration
-  lc_name = "mysql-lc"
+  lc_name = "${var.appname}-lc"
   image_id        = "${var.ami}"
   instance_type   = "${var.instance_type}"
-  security_groups = ["${aws_security_group.privateDB.id}"]
+  security_groups = ["${aws_security_group.public.id}"]
 
  
   # Auto scaling group
-  asg_name                  = "mysql-asg"
-  vpc_zone_identifier       = ["${aws_subnet.privateDB.id}"]
+  asg_name                  = "${var.appname}-asg"
+  vpc_zone_identifier       = ["${aws_subnet.public.id}"]
   health_check_type         = "EC2"
-  min_size                  = 3
-  max_size                  = 6
-  desired_capacity          = 6
-  wait_for_capacity_timeout = 3
+  min_size                  = "${var.min_app_size}"
+  max_size                  = "${var.max_app_size}"
+  desired_capacity          = "${var.desired_app_capacity}"
+  wait_for_capacity_timeout = 0
   tags_as_map = {
-    Name       = "${var.Name}.-Mysql"
-    Env        = "${var.Env}"
-    Created_by = "${var.Created_by}"
-    Dept       = "${var.Dept}"
+      Name = "${var.Name}"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
   }
-
-  
 }
